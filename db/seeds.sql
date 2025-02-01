c9004b80706b15b3058abed0f09a8185c71cadaa
taijamartinez <taijasmartinez@gmail.com>
2025-02-01 14:07:42 -0500

    added seeds.sql code

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,18 @@
 INSERT INTO department(name)
-VALUES();
+VALUES
+('Engineering'),
+('Sales'),
+('HR');
 
-INSERT INTO role (title, salary)
-VALUES();
 
-INSERT INTO employee(first_name, last_name)
-VALUES();
+INSERT INTO role (title, salary, department_id)
+VALUES
+('Software Engineer', 100000, 1),
+('Sales Manager', 80000, 2),
+('HR Specialist', 60000, 3);
+
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
+VALUES
+('Aaron', 'Johnson', 1, NULL),
+('Bill', 'Smith', 2, NULL),
+('Carly', 'Brown', 3, NULL);
